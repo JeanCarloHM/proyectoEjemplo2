164449af6357ae718ac75f2ab6416c4e0465b886
Jean Carlo Hincapie <jeancarlo1202@gmail.com>
2019-07-21 17:12:41 -0500

    Second Commit

---
+++ facelectronicaapi/facelectronicaapi/Models/JsonResponseModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FacElectronicaApi.Models
+{
+    /// <summary>
+    /// Clase utilizada para establecer un formato de respuesta de las solicitudes del Api
+    /// </summary>
+    public class JsonResponseModel
+    {
+        /// <summary>
+        /// Datos de respuesta de la petición
+        /// </summary>
+        public Object Data { get; set; } = null;
+
+        /// <summary>
+        /// Indica si la petición finalizó correctamente o no
+        /// </summary>
+        public Boolean IsValid { get; private set; } = true;
+
+        /// <summary>
+        /// Mensaje de respuesta de la petición
+        /// </summary>
+        public String Message { get; set; } = null;
+
+        /// <summary>
+        /// Exepción de la petición
+        /// </summary>
+        public Exception Exception { get; private set; } = null;
+
+        /// <summary>
+        /// Adiciona una exepción a la respuesta del Api
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <param name="message"></param>
+        public void CreateExeception(Exception ex, string message = null)
+        {
+            this.IsValid = false;
+
+            this.Exception = Utils.UtilException.GetInnerException(ex);
+
+            if (!string.IsNullOrWhiteSpace(message))
+            {
+                this.Message = message;
+            }
+            else
+            {
+                this.Message = ex.Message;
+            }
+        }
+    }
+}
---
+++ facelectronicaapi/facelectronicaapi/Provider/AuthorizationServerProvider.cs
@@ -0,0 +1,71 @@
+﻿using FacElectronicaApi.DBContext;
+using FacElectronicaApi.DBContext.Entities;
+using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace FacElectronicaApi.Provider
+{
+    public class AuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        /// <summary>
+        /// Valida la autenticación y en este caso dejamos que sea el propio OAuth quien haga la validación
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        /// <summary>
+        /// Valida los datos del usuario que se esta logueando y retorna el token
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            try
+            {
+                Boolean esValido = false;
+                context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+                using(UnitOfWork unitOfWork = new UnitOfWork())
+                {
+                    Decimal nitEmpresa = Convert.ToDecimal(context.UserName);
+
+                    EmpresasEntity empresa = unitOfWork.EmpresasRepository.Obtener(nitEmpresa);
+
+                    if (empresa != null)
+                    {
+                        if (empresa.Password == context.Password)
+                        {
+                            esValido = true;
+                        }
+                    }
+                }
+
+                if (esValido)
+                {
+                    var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+                    identity.AddClaim(new Claim("sub", context.UserName));
+                    identity.AddClaim(new Claim("role", "user"));
+
+                    context.Validated(identity);
+                }
+                else
+                {
+                    context.SetError("invalid_grant", "Usuario o contraseña incorrecta");
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                var exception = Utils.UtilException.GetInnerException(ex);
+                context.SetError(exception.Message);
+            }
+        }
+    }
+}
